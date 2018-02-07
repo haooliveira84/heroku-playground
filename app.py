ca5d2e2fbdf8ba2f3108e102fa18f06fff6eb46f
Henrique Antonio de Oliveira <henrique.oliveira@byseven.com.br>
2018-02-07 16:17:35 -0200

    Remove modules unsed and make json return

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import json
 import requests
 import flask
@@ -9,22 +12,32 @@ app = flask.Flask(__name__)
 
 def macaddr(macaddr=None):
 	if flask.request.method == 'POST':
-    			json_dict = flask.request.get_json()
-			if flask.request.headers.get('macaddr'):
-    				macaddr = flask.request.headers.get('macaddr')
-			else:
-					macaddr = json_dict['macaddr']
+		json_dict = flask.request.get_json()
+		if flask.request.headers.get('macaddr'):
+				macaddr = flask.request.headers.get('macaddr')
+		else:
+				macaddr = json_dict['macaddr']
 	else:
-			if flask.request.args.get('macaddr'):
-					macaddr = flask.request.args.get('macaddr')
-	response = connected_Database(macaddr)
+		if flask.request.args.get('macaddr'):
+				macaddr = flask.request.args.get('macaddr')
+	response = mac_consult(macaddr)
 	return response
 
 def mac_consult(macaddr):
-    response = {}
-    url = 'http://macvendors.co/api/jsonp/'
-    response = requests.get(ural+macaddr)
-    return response
+	response = {}
+	url = 'http://macvendors.co/api/jsonp/'
+	try:
+		req_response = requests.get(url+macaddr)
+	except:
+		response = json.dumps({'status':'error','message':'Error'})
+	finally:
+		if req_response == None:
+			status_code=404
+			response=json.dumps({'code':status_code,'status':'error','message':'MAC Not Found!'})
+		else:
+			status_code=200
+			response = req_response
+	return json_view(response, status_code)
 
 def json_view(response, status_code):
 	response = flask.Response(
@@ -33,11 +46,6 @@ def json_view(response, status_code):
 		mimetype='application/json'
 	)
 	return response
-
-def datetime_converter(obj):
-    if isinstance(obj, datetime.datetime):
-    		return obj.__str__()
-
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
